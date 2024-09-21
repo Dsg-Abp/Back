aba5b55a51fb229853f8f6c75bbd22ced2fc4e8e
Isaac - Exon <isaac.souza@exongenomics.com>
2024-09-21 11:12:24 -0300

    criação da rorta para profile

--- src/Rotas/index.ts
+++ src/Rotas/index.ts
@@ -6,7 +6,7 @@ import EmailController from "../Controladores/PostResetSenha";
 import { authenticateToken } from "../Controladores/authMiddleware";
 import Alimentos from "../Controladores/AlimentosController";
 import HealthPass from "../Controladores/healthPass";
-import GetHealthPass from "../Controladores/getHealthPass";
+import Profile from "../Controladores/Profile";
 
 const router = express.Router();
 
@@ -14,18 +14,21 @@ const authController = new AuthController();
 const cadastro = new Cadastro();
 const redefinicaoSenha = new RedefinicaoSenha();
 const emailController = new EmailController();
-const alimentosController = new Alimentos(); //rota adicionada para procurar dados no banco;
+const alimentosController = new Alimentos();
+const newProfile = new Profile();
 
 // Definindo a rota para buscar alimentos
-router.post('/buscar-alimento', alimentosController.Find);
+router.post("/buscar-alimento", alimentosController.Find);
 const healthPass = new HealthPass();
-const getHealthPass = new GetHealthPass();
 
 // Rota de login
 router.post("/login", authController.login);
 
 // Rota de cadastro de usuário (registro)
 router.post("/register", cadastro.register);
+
+// Rota para salvar os dados do perfil
+router.post("/save-profile", newProfile.saveProfile);
 
 // Rota de recuperação de senha
 router.post("/recuperacao-senha", redefinicaoSenha.resetPassword);
@@ -41,16 +44,6 @@ router.get("/auth/google", authController.googleLogin);
 // Rota de callback do Google
 router.get("/auth/google/callback", (req, res) =>
   authController.googleCallback(req, res)
-);
-
-// Rota para obter contagem de passos
-router.get("/user/:userId/step-count", (req, res) =>
-  healthPass.getStepCount(req, res)
-);
-
-//Rota para trazer da coleção instatanea -  somar e exibir por dia.
-router.get("/user/:userId/steps-today", (req, res) =>
-  getHealthPass.getStepCount(req, res)
 );
 
 export default router;
