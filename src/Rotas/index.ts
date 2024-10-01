1979d95acf42817b33baaafecbc7201c37f79045
MingRenan <100416045+MingRenan@users.noreply.github.com>
2024-09-30 21:59:14 -0300

    Revert "Merge branch 'main' of https://github.com/Dsg-Abp/Back"
    This reverts commit 7304bc88fe18aa3fff0846592ec60d86921ff9a8, reversing
    changes made to 5243a3c53b2d0a9578016ac40921625dc84f2b98.

--- src/Rotas/index.ts
+++ src/Rotas/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
-import AuthController from "../Controladores/PostLogin"; // Normal login
-
+import AuthController from "../Controladores/PostLogin";
 import Cadastro from "../Controladores/Cadastro";
 import RedefinicaoSenha from "../Controladores/RecuperaçãoSenha";
 import EmailController from "../Controladores/PostResetSenha";
@@ -9,38 +8,32 @@ import Alimentos from "../Controladores/AlimentosController";
 import HealthPass from "../Controladores/healthPass";
 import Profile from "../Controladores/Profile";
 import WebCam from "../Controladores/WebCam";
+
 import Agua from "../Controladores/UpWater";
-import GoogleAuthController from "../Controladores/PostLoginGoogle";
 
 const router = express.Router();
 
 const authController = new AuthController();
-const googleAuthController = new GoogleAuthController(); // Instantiate GoogleAuthController
 const cadastro = new Cadastro();
+
+//parte da agua
 const agua = new Agua();
+
+
 const redefinicaoSenha = new RedefinicaoSenha();
 const emailController = new EmailController();
 const alimentosController = new Alimentos();
 const newProfile = new Profile();
 const photo = new WebCam();
-const healthPass = new HealthPass();
 
 // Definindo a rota para buscar alimentos
 router.post("/buscar-alimento", alimentosController.Find);
+const healthPass = new HealthPass();
 
 // Rota de login
 router.post("/login", authController.login);
 
-// Rota de autenticação com Google
-router.get("/auth/google", googleAuthController.googleLogin); // Update to use GoogleAuthController
-
-// Rota de callback do Google
-router.get(
-  "/auth/google/callback",
-  (req, res) => googleAuthController.googleCallback(req, res) // Update to use GoogleAuthController
-);
-
-// Rota de inserção de água
+//rota de insercao de agua
 router.post("/agua", agua.registera);
 router.post("/listagua", agua.FindAgua);
 router.get("/teste", agua.findTeste);
@@ -49,7 +42,7 @@ router.post("/insert", agua.upTest);
 // Rota de cadastro de usuário (registro)
 router.post("/register", cadastro.register);
 
-// Rota para salvar os dados do perfil
+// Rota para salvar os dados do perfil//
 router.post("/profile", newProfile.saveProfile);
 
 // Rota de recuperação de senha
@@ -60,11 +53,19 @@ router.post("/reset", (req, res) =>
   emailController.enviarEmailDeRecuperacao(req, res)
 );
 
+// Rota para autenticação com Google
+router.get("/auth/google", authController.googleLogin);
+
+// Rota de callback do Google
+router.get("/auth/google/callback", (req, res) =>
+  authController.googleCallback(req, res)
+);
+
 // Rota para obter os dados do perfil usando o userId
 router.get("/profile/:userId", newProfile.getProfile);
 
-// Rota para salvar foto da webcam
 router.post("/web", photo.savePhoto);
+
 router.get("/web", photo.getCam);
 
 export default router;
