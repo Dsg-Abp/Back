24896e187a282b12f11f7ccedc0fb474c3065ffb
Fillipe Almeida <120722022+FPbueno@users.noreply.github.com>
2024-10-28 21:58:12 -0300

    atualização no inserData de alimentos

--- src/Controladores/UserAlimentosController.ts
+++ src/Controladores/UserAlimentosController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+import { MongoClient, Db } from "mongodb";
 dotenv.config();
 
 const DB_NAME = process.env.DB_NAME_INSERT;
 const MONGO_URI = process.env.MONGO_URI;
+
+let client: MongoClient;
+let db: Db;
+
+async function connectToDatabase() {
+  if (!client) {
+    client = new MongoClient(MONGO_URI!);
+    await client.connect();
+    db = client.db(DB_NAME);
+    console.log("Conexão com o MongoDB estabelecida.");
+  }
+  return db;
+}
 
 class UserAlimentos {
   // Método para inserir dados no banco de dados
@@ -17,40 +30,25 @@ class UserAlimentos {
       return;
     }
 
-    let client: MongoClient | null = null;
-
     try {
-      // Conectando ao MongoDB
-      client = new MongoClient(MONGO_URI!);
-      await client.connect();
-      const db = client.db(DB_NAME);
+      const db = await connectToDatabase();
       const collection = db.collection("userData");
 
       // Inserindo os dados
       const result = await collection.insertOne({ alimentos, nutrientes, gramas, refeicao });
       
       // Respondendo com sucesso
-      res.status(201).json({ message: "Dados inseridos com sucesso", data: result });
+      res.status(201).json({ message: "Dados inseridos com sucesso", insertedId: result.insertedId });
     } catch (error) {
       console.error("Erro ao inserir dados:", error);
       res.status(500).json({ message: "Erro ao inserir os dados" });
-    } finally {
-      // Certifique-se de fechar a conexão com o banco de dados
-      if (client) {
-        await client.close();
-      }
     }
   }
 
   // Método para buscar dados do banco de dados
   async getData(req: Request, res: Response): Promise<void> {
-    let client: MongoClient | null = null;
-
     try {
-      // Conectando ao MongoDB
-      client = new MongoClient(MONGO_URI!);
-      await client.connect();
-      const db = client.db(DB_NAME);
+      const db = await connectToDatabase();
       const collection = db.collection("userData");
 
       // Buscando os dados
@@ -61,11 +59,6 @@ class UserAlimentos {
     } catch (error) {
       console.error("Erro ao buscar dados:", error);
       res.status(500).json({ message: "Erro ao buscar os dados" });
-    } finally {
-      // Fechando a conexão com o banco de dados
-      if (client) {
-        await client.close();
-      }
     }
   }
 }
